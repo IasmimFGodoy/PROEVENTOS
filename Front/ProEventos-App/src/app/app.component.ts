5c43d9ce8b84ba3b741a7c990ec9d7b3f7bf6255
iasmimgodoy <iasmimgodoy@outlook.com>
2023-05-19 00:08:56 -0300

    Instalaçao do Bootstrap no projeto, Request HTTP no componente de eventos, configuração do CORS na API de evento 19052023

---
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: `<h1>olá mundo</h1>`,
+
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ProEventos-App';
+}
